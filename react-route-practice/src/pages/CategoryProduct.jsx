67bcc8fdb9c3797965f89a56e47d8ae7e4dd7039
Haseebwaqas489 <waqashaseeb57@gmail.com>
2026-09-03 15:54:17 +0500

    use Url Vlaues

--- react-route-practice/src/pages/CategoryProduct.jsx
+++ react-route-practice/src/pages/CategoryProduct.jsx
@@ -1,22 +1,28 @@
 
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
 
 function CategoryProduct() {
 
   const { categoryId, productId } = useParams();
 
+  const [searchParams] = useSearchParams();
+
   useEffect(() => {
     console.log("Category:", categoryId);
     console.log("Product ID:", productId);
-  }, [categoryId, productId]);
+    console.log("Color:", searchParams.get("color"));
+  }, [categoryId, productId, searchParams]);
 
   return (
     <div>
       <h1>Product Details</h1>
 
       <h2>Category: {categoryId}</h2>
+
       <h2>Product ID: {productId}</h2>
+
+      <h2>Color: {searchParams.get("color")}</h2>
     </div>
   );
 }
